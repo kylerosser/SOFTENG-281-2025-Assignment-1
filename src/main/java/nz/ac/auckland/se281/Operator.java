21511133ff846236dbd63855372566697e88f53b
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-03-30 16:54:30 +1300

    change class Operator.java to use getters and setters instead of public fields

--- src/main/java/nz/ac/auckland/se281/Operator.java
+++ src/main/java/nz/ac/auckland/se281/Operator.java
@@ -2,13 +2,37 @@ package nz.ac.auckland.se281;
 
 public class Operator {
 
-  public String name;
-  public Types.Location location;
-  public String operatorID;
+  private String name;
+  private Types.Location location;
+  private String operatorID;
   
   public Operator(String name, Types.Location location, String operatorID) {
     this.name = name;
     this.location = location;
+    this.operatorID = operatorID;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Types.Location getLocation() {
+    return location;
+  }
+
+  public void setLocation(Types.Location location) {
+    this.location = location;
+  }
+
+  public String getOperatorID() {
+    return operatorID;
+  }
+
+  public void setOperatorID(String operatorID) {
     this.operatorID = operatorID;
   }
 }
