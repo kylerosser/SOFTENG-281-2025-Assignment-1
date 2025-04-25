f32704ed2fb93860981f9564a7e227f7542d9976
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-04-25 17:41:12 +1200

    modify printEntry method of PrivateReview class to correctly print private reviews

--- src/main/java/nz/ac/auckland/se281/PrivateReview.java
+++ src/main/java/nz/ac/auckland/se281/PrivateReview.java
@@ -3,6 +3,8 @@ package nz.ac.auckland.se281;
 public class PrivateReview extends Review {
   private String email;
   private boolean followupRequested;
+  private String followupResponse;
+  private boolean resolved;
 
   public PrivateReview(
     Activity activity, 
@@ -15,10 +17,28 @@ public class PrivateReview extends Review {
     super(activity, name, rating, comment);
     this.email = email;
     this.followupRequested = followupRequested;
+    this.followupResponse = "";
+    this.resolved = false;
   }
   
   public void printEntry() {
-    
+    MessageCli.REVIEW_ENTRY_HEADER.printMessage(
+      Integer.toString(this.rating),
+      "5",
+      "Private",
+      this.reviewId,
+      this.name
+    );
+    MessageCli.REVIEW_ENTRY_REVIEW_TEXT.printMessage(this.comment);
+    if (this.followupRequested) {
+      if (this.resolved) {
+        MessageCli.REVIEW_ENTRY_RESOLVED.printMessage(this.followupResponse);
+      } else {
+        MessageCli.REVIEW_ENTRY_FOLLOW_UP.printMessage(this.email);
+      }
+    } else {
+      MessageCli.REVIEW_ENTRY_RESOLVED.printMessage("-");
+    }
   }
 
   public String getEmail() {
@@ -35,6 +55,22 @@ public class PrivateReview extends Review {
 
   public void setFollowupRequested(boolean followupRequested) {
     this.followupRequested = followupRequested;
+  }
+
+  public String getFollowupResponse() {
+    return followupResponse;
+  }
+
+  public void setFollowupResponse(String followupResponse) {
+    this.followupResponse = followupResponse;
+  }
+
+  public boolean isResolved() {
+    return resolved;
+  }
+
+  public void setResolved(boolean resolved) {
+    this.resolved = resolved;
   }
   
 }
